ef56c822fcc45e41637bdabf3732dd63b6bf8e61
Bharat Reddy <rikred@Bobs-Mac.local>
2023-10-07 14:59:02 +0530

    calculator app nicely done with no help

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,64 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(array) {
+	
+  let sumedValue=0;
+
+  array.forEach(sumFunc);
+  //for each takes function to be done on each item
+  function sumFunc(item) {
+      sumedValue += item;
+  }
+
+      return sumedValue;
+};
+
+const multiply = function(array) {
+  let multipiledResult = 1;
+                    
+  array.forEach(multiplier);
+
+  function multiplier(item){
+      multipiledResult *= item;
+  }
+
+  console.log(`from inside the multiply function ${ multipiledResult }`)
+  return multipiledResult;
+
+};
+
+const power = function(a,x) {
+
+
+  let powerOf=1;
+  for (let i = 0; i < x; i++) {
+
+
+      powerOf *=a;
+      
+  }
+  console.log(`from inside the power function ${ powerOf }`)
+  return powerOf;
+
 	
 };
 
-const subtract = function() {
+const factorial = function(a) {
 	
-};
+  let factResult=1;
+  for (let i = 1; i <= a; i++) {
+       factResult *=i;
+      
+  }
+  console.log(`from inside the factorial function ${ factResult }`)
+  return factResult;
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
