526b9f8335384b96ef0baa29a16b5f4f311e6b90
Bharat Reddy <rikred@Bobs-Mac.local>
2023-10-07 11:03:06 +0530

    checked divisiblity by 400, 100, 4 then evaluatd if they are leap years

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
 
-};
+    if (!(typeof x)==='number'&&!(typeof y)==='number') {
+
+        return('ERROR');
+    }
+
+    if (x<0||y<0) {
+
+        return('ERROR');
+
+    }
+
+    if(x>y)
+    {
+        const t=x;
+        x=y;
+        y=t;
+    }
+    
+    let sum = 0
+    for (let i = x; i <= y; i++) {
+        sum = sum +i;
+        
+    }
+
+    return sum;
+
+
+
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
